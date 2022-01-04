69535f825b56f876dedf821954a1581e6e3d964e
James DiRosa <jdirosa@barracuda.com>
2022-01-03 20:54:54 -0500

    Updates

---
+++ precache-manifest.b840a48e62e1143590919e0d9c28a550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb736e4e2d3fae11c15b3807d95c9e7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3b30817b01511dbe03c",
+    "url": "./static/js/2.7ea47cb1.chunk.js"
+  },
+  {
+    "revision": "f9149ae0f0977793c838a92fcf8ae78b",
+    "url": "./static/js/2.7ea47cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a53b81e36e2bb4d6246",
+    "url": "./static/js/main.c9fd872e.chunk.js"
+  },
+  {
+    "revision": "dd3e15f85e48bf5c4816",
+    "url": "./static/js/runtime-main.a52dd606.js"
+  }
+]);
